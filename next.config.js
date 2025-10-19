d5e60d0ebcc97ae9e014e255892c84739bae9675
preshgit <preciousdesk10@gmail.com>
2025-10-20 00:22:57 +0100

    Config - Tailwind installation

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "**",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: '**',
+        pathname: '/**',
       },
     ],
   },
+  // ✅ Correct placement
+  outputFileTracingRoot: path.join(__dirname),
 };
 
 module.exports = nextConfig;
